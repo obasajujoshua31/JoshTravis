e63befc1bc7f4bb1ccf9c524da42b21a515f986e
Joshua <obasajujoshua31@gmail.com>
2018-11-06 17:58:11 +0100

    new stuff

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,8 @@ var _data = require('./data');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var PORT = process.env.PORT || 5000;
+
 var app = (0, _express2.default)();
 app.use(_express2.default.json());
 app.use(_express2.default.urlencoded({ extended: false }));
